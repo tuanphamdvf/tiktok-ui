4bf08081359b85c21be4e2490b260f9ee86231be
tuanphamdvf <104239862+tuanphamdvf@users.noreply.github.com>
2022-05-26 14:08:54 +0700

    Router/Layout

---
+++ src/components/pages/ProFile/index.js
@@ -0,0 +1,5 @@
+function Profile() {
+    return <h2>ProFile Page</h2>;
+}
+
+export default Profile;
---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import Home from '~/components/pages/Home';
+import Following from '~/components/pages/Following';
+import Profile from '~/components/pages/ProFile';
+import Upload from '~/components/pages/Upload';
+import { HeaderOnly } from '~/components/Layout';
+import Search from '~/components/pages/Search';
+
+//public route
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/Following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+const privatePoutes = [];
+export { publicRoutes, privatePoutes };
